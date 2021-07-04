b0704442a5b6b47891f631b4f225c0f0d387e3b9
slezakt <info@tomasslezak.cz>
2021-07-04 17:21:32 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
     },
     extend: {
       colors: {},
+      boxShadow: {
+        blue: '0 20px 25px -5px rgba(59, 130, 246, 0.1), 0 10px 10px -5px rgba(59, 130, 246, 0.04)',
+      },
     },
   },
   variants: {},
